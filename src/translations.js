f155475b2b76159941fc951f84d1209c3f92b78e
Arthur Zheng <arthurzheng150@gmail.com>
2026-05-02 13:18:38 +0800

    init

---
+++ src/translations.js
@@ -0,0 +1,75 @@
+/* eslint-disable quote-props */
+/* eslint-disable max-len */
+/**
+ * Translation file for this resouce.
+ *
+ * IMPORTANT:
+ * - The "en" (English) section is automatically generated from source files.
+ *   Do NOT modify the "en" section manually.
+ * - Other language sections (e.g., "zh-cn", "zh-tw", "ja", etc.) should be
+ *   manually translated by you.
+ * - When you run the extraction script again, only the "en" section will be
+ *   updated. Your manual translations in other languages will be preserved.
+ *
+ * Structure:
+ * - interface: translations for name/description (used by GUI formatMessage)
+ * - extensions: translations for extension blocks (used by VM formatMessage)
+ * - blocks: translations for Blockly blocks (used by Blockly.Msg)
+ */
+
+export default {
+    'interface': {
+        'en': {
+            'eightDigitDisplay.name': '8-Digit Display',
+            'eightDigitDisplay.description': '8-digit display module based on MAX7219.'
+        },
+        'ru': {
+            'eightDigitDisplay.name': '8-Digit Display',
+            'eightDigitDisplay.description': '8-digit display module based on MAX7219.'
+        },
+        'zh-cn': {
+            'eightDigitDisplay.name': '8-Digit Display',
+            'eightDigitDisplay.description': '8-digit display module based on MAX7219.'
+        },
+        'zh-tw': {
+            'eightDigitDisplay.name': '8-Digit Display',
+            'eightDigitDisplay.description': '8-digit display module based on MAX7219.'
+        }
+    },
+    'extensions': {
+        'en': {},
+        'ru': {},
+        'zh-cn': {},
+        'zh-tw': {}
+    },
+    'blocks': {
+        'en': {
+            'EIGHTDIGITDISPLAY_CATEGORY': '8-Digit Display',
+            'EIGHTDIGITDISPLAY_INIT': 'init 8-digit display pin DIN %1 CS %2 CLK %3',
+            'EIGHTDIGITDISPLAY_DISPLAYNUMBER': '8-digit display show number %1 decimal %2',
+            'EIGHTDIGITDISPLAY_SETBRIGHTNESS': 'set 8-digit display brightness %1',
+            'EIGHTDIGITDISPLAY_CLEAR': 'clear 8-digit display'
+        },
+        'ru': {
+            'EIGHTDIGITDISPLAY_CATEGORY': '8-Digit Display',
+            'EIGHTDIGITDISPLAY_INIT': 'init 8-digit display pin DIN %1 CS %2 CLK %3',
+            'EIGHTDIGITDISPLAY_DISPLAYNUMBER': '8-digit display show number %1 decimal %2',
+            'EIGHTDIGITDISPLAY_SETBRIGHTNESS': 'set 8-digit display brightness %1',
+            'EIGHTDIGITDISPLAY_CLEAR': 'clear 8-digit display'
+        },
+        'zh-cn': {
+            'EIGHTDIGITDISPLAY_CATEGORY': '8-Digit Display',
+            'EIGHTDIGITDISPLAY_INIT': 'init 8-digit display pin DIN %1 CS %2 CLK %3',
+            'EIGHTDIGITDISPLAY_DISPLAYNUMBER': '8-digit display show number %1 decimal %2',
+            'EIGHTDIGITDISPLAY_SETBRIGHTNESS': 'set 8-digit display brightness %1',
+            'EIGHTDIGITDISPLAY_CLEAR': 'clear 8-digit display'
+        },
+        'zh-tw': {
+            'EIGHTDIGITDISPLAY_CATEGORY': '8-Digit Display',
+            'EIGHTDIGITDISPLAY_INIT': 'init 8-digit display pin DIN %1 CS %2 CLK %3',
+            'EIGHTDIGITDISPLAY_DISPLAYNUMBER': '8-digit display show number %1 decimal %2',
+            'EIGHTDIGITDISPLAY_SETBRIGHTNESS': 'set 8-digit display brightness %1',
+            'EIGHTDIGITDISPLAY_CLEAR': 'clear 8-digit display'
+        }
+    }
+};
